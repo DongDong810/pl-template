fff5ebaff0b0b711b4d34008e227e6fb3eb58b78
DY112 <dongyoung.kim@yonsei.ac.kr>
2023-09-26 15:07:51 +0900

    remove base_solver, modify cfg args

--- solvers/model_name_solver_ver.py
+++ solvers/model_name_solver_ver.py
@@ -1,3 +1,4 @@
+import pytorch_lightning as pl
 import torch,wandb,random
 import torch.nn as nn
 import os.path as osp
@@ -5,10 +6,11 @@ import numpy as np
 from omegaconf import OmegaConf
 from solvers.base_solver import BaseSolver
 from datasets.example_dataset import get_loader
+from utils.setup import get_optimizer, get_scheduler
 
-class Solver(BaseSolver):
+class Solver(pl.LightningModule):
     def __init__(self,net,loss,**cfg):
-        super().__init__(net=net,loss=loss,**cfg)
+        super().__init__()
         self.save_hyperparameters(ignore=['net','loss'])
         self.cfg = OmegaConf.create(cfg)
         self.net = net
@@ -58,3 +60,24 @@ class Solver(BaseSolver):
     
     def test_step(self, batch_dict, batch_idx):
         self.validation_step(batch_dict,batch_idx)
+
+    def configure_optimizers(self):
+        # get optimizer and scheduler
+        optimizer_mode = self.cfg.train.optimizer.mode
+        optimizer = get_optimizer(opt_mode=optimizer_mode,
+                                  net_params=self.net.parameters(),
+                                  **(self.cfg.train.optimizer[optimizer_mode]))
+        scheduler_mode = self.cfg.train.scheduler.mode
+        scheduler = get_scheduler(sched_mode=scheduler_mode,
+                                  optimizer=optimizer,
+                                  **(self.cfg.train.scheduler[scheduler_mode]))
+        
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': {
+                'scheduler': scheduler,
+                'interval': 'epoch',
+                'frequency': self.cfg.train.check_val_every_n_epoch,
+                'monitor': self.cfg.train.scheduler.monitor,
+            }
+        }
